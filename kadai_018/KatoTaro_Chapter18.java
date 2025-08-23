37558ceb0deadf82f91878552bfdf3293e17c8a7
masaki163 <masaki163d@gmail.com>
2025-08-23 23:36:57 +0900

    Update KatoTaro_Chapter18.java

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -2,8 +2,8 @@ package kadai_018;
 
 public class KatoTaro_Chapter18 extends Kato_Chapter18 {
 	
-	public KatoTaro_Chapter18() {
-		setGivenName("太郎");
+	public void setGivenName() {
+		this.givenName = "太郎";
 	}
 
 	public void eachIntroduce() {
