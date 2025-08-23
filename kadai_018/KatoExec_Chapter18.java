c9538218bd5ff4d9871e5c260831ea110a44e3d9
masaki163 <masaki163d@gmail.com>
2025-08-23 21:43:15 +0900

    Update KatoExec_Chapter18.java

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -8,11 +8,7 @@ public class KatoExec_Chapter18 {
 		KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
 		KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
 		
-		Taro.setGivenName("太郎");
-		Ichiro.setGivenName("一郎");
-		Hanako.setGivenName("花子");
-		
-		
+
 		Taro.execIntroduce();
 		Ichiro.execIntroduce();
 		Hanako.execIntroduce();
