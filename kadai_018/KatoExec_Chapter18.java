f981d83c847ca91d3317acc3a9e981fc6cb16b4b
masaki163 <masaki163d@gmail.com>
2025-08-23 20:39:02 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,22 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		
+		KatoTaro_Chapter18 Taro = new KatoTaro_Chapter18();
+		KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
+		KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
+		
+		Taro.givenName = "太郎";
+		Ichiro.givenName = "一郎";
+		Hanako.givenName = "花子";
+		
+		
+		Taro.execIntroduce();
+		Ichiro.execIntroduce();
+		Hanako.execIntroduce();
+		
+	}
+
+}
