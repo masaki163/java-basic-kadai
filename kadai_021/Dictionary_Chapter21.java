98b3a78ab2a8f5d80e2dda52a8355425687f9383
masaki163 <masaki163d@gmail.com>
2025-08-30 14:11:33 +0900

    Add files via upload

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,31 @@
+package kadai_021;
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	
+	private HashMap<String, String> dictionaryMap = new HashMap<>();
+	
+
+	public Dictionary_Chapter21() {
+    	dictionaryMap.put("apple","りんご");
+    	dictionaryMap.put("peach","桃");
+    	dictionaryMap.put("banana","バナナ");
+    	dictionaryMap.put("lemon","レモン");
+    	dictionaryMap.put("pear","梨");
+    	dictionaryMap.put("kiwi","キウィ");
+    	dictionaryMap.put("strawberry","いちご");
+    	dictionaryMap.put("grape","ぶどう");
+    	dictionaryMap.put("muscat","マスカット");
+    	dictionaryMap.put("cherry","さくらんぼ");
+	}
+    
+    public void searchWords(String[] words) {
+        for (String word : words) {
+            if (dictionaryMap.containsKey(word)) {
+                System.out.println(word + "の意味は" + dictionaryMap.get(word));
+            } else {
+                System.out.println(word + "は辞書には存在しません");
+            }
+        }
+    }
+}
